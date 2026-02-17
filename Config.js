e6dcdac298572651a27cf5efc4240345ce7a03ca
Realshi <govv.maill@gmail.com>
2026-02-17 12:33:18 -0500

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,81 @@
+// first time touching JS
+
+const SITE_CONFIG = {
+
+    title: "Water Tag Modded",
+
+    sections: [
+        {
+            name: "Settings",
+            features: [
+                "Right Hand Menu",
+                "Notifications",
+                "Clear Notifications",
+                "Disconnect Button",
+                "Button Mode: Triggers / Normal",
+                "Menu Type: GUI / Holdable"
+            ]
+        },
+        {
+            name: "Movement",
+            features: [
+                "Scale Control",
+                "Slide Control",
+                "Fly",
+                "Excel Fly",
+                "Trigger Fly",
+                "Joystick Fly",
+                "Speed Boost",
+                "No Clip",
+                "Platforms",
+                "Platform Gun",
+                "Checkpoint",
+                "Force Tag Freeze",
+                "No Tag Freeze",
+                "TP Gun",
+                "Rig Gun",
+                "Up And Down"
+            ]
+        },
+        {
+            name: "Player",
+            features: [
+                "Unlock All",
+                "Loud Hand Taps",
+                "Silent Hand Taps",
+                "Ghost Monkey",
+                "Invis Monkey"
+            ]
+        },
+        {
+            name: "Visuals",
+            features: [
+                "Beacons",
+                "Chams"
+            ]
+        },
+        {
+            name: "Photon",
+            features: [
+                "Join Random",
+                "Reconnect",
+                "No Tag On Join",
+                "Disable AFK Kick"
+            ]
+        },
+        {
+            name: "Overpowered",
+            features: [
+                "Tag All",
+                "Set Master",
+                "Slow All",
+                "Mat All",
+                "Vibrate All",
+                "Crash All",
+                "Crash Gun",
+                "Earrape (NOT NETWORKED)"
+            ]
+        }
+    ]
+
+};
